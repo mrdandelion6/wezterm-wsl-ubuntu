2c256359eec2fe994afb54370ee00636d13326a4
mrdandelion6 <faisal.shaik@mail.utoronto.ca>
2025-01-10 23:14:44 -0500

    added wezterm config

---
+++ wezterm.lua
@@ -0,0 +1,7 @@
+local wezterm = require 'wezterm'
+local config = {
+    font = wezterm.font("3270 Nerd Font Mono"),
+    font_size = 12.5,
+    enable_tab_bar = false,  -- This will hide the tab bar
+}
+return config
