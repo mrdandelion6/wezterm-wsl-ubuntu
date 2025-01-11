9f18c9c555d22937a14264efb0a355b03c53bc1e
faisal375 <144740367+faisal375@users.noreply.github.com>
2025-01-11 00:03:17 -0500

    changed font, removed mono

--- wezterm.lua
+++ wezterm.lua
@@ -1,6 +1,6 @@
 local wezterm = require 'wezterm'
 local config = {
-    font = wezterm.font("3270 Nerd Font Mono"),
+    font = wezterm.font("3270 Nerd Font"),
     font_size = 12.5,
     enable_tab_bar = false,  -- This will hide the tab bar
 }
